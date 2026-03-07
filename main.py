2f2957f2f6ba42d3e95c7f3dafa16d4a14284a44
Cassio Farias <cassiofarias@outlook.com>
2026-03-07 16:11:16 -0300

    delete de telplate Resultado não utilizado

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from logica.calculadora import OperacaoPortabilidade
 from datetime import datetime
 from decimal import Decimal
@@ -8,20 +8,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('complexo.html')
+    return redirect(url_for('simulador_portabilidade'))
 
-@app.route('/complexo', methods=['GET','POST'])
-def complexo():
+@app.route('/simulador', methods=['GET','POST'])
+def simulador_portabilidade():
     resultado = None
     nova_parcela = prazo = juros = data_pagamento_str = None
     contratos = []
     dados_contratos=[]
+    parcelas = []
+
+    if nova_parcela is not None:
+        parcela_total = nova_parcela + sum(parcelas)
+    else:
+        parcela_total = 0
 
     if request.method == 'POST':
         try:
             nomes_bancos = [n for n in request.form.getlist('nome_banco')]
             num_contratos = [n for n in request.form.getlist('num_contrato')]
             parcelas = [Decimal(p) for p in request.form.getlist('valor_parcelas')]
+            parcela_total = parcela_total 
             saldos = [Decimal(s) for s in request.form.getlist('saldos')]
             nova_parcela = Decimal(request.form.get('nova_parcela'))
             prazo = int(request.form.get('prazo'))
@@ -57,11 +64,11 @@ def complexo():
 
     # esse return é necessário para não apagar os dados preenchidos antes dos contratos
     return render_template(
-        'complexo.html',
+        'simulador.html',
         dados_contratos=dados_contratos,
         resultado=resultado,
         nova_parcela = nova_parcela,
-        parcela_total = nova_parcela + sum(parcelas),
+        parcela_total = parcela_total,
         prazo=prazo,
         juros=juros,
         data_pagamento=data_pagamento_str,
@@ -116,7 +123,7 @@ def calcular():
 
     # esse return é necessário para não apagar os dados preenchidos antes dos contratos
     return render_template(
-        'complexo.html',
+        'simulador.html',
         dados_contratos=dados_contratos,
         resultado=resultado,
         nova_parcela=nova_parcela,
